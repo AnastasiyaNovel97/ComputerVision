376bc71a660bbb8080dbe523b644e838303f98ad
Анастасия Рогалёва <novel.nastya@yandex.ru>
2020-06-25 10:59:03 +0700

    Lab6 & Lab7 добавила доп. тесты

--- Lab6-7/Lab6-7/main.cpp
+++ Lab6-7/Lab6-7/main.cpp
@@ -30,8 +30,8 @@ void lab67(QString path,QString fileName1, QString ext1,QString fileName2, QStri
     QVector<Descriptor> descriptors2 = myDescr2.GetDescriptors();
 
     Print("\nИщем похожие дескрипторы изображений.");
-    //QImage result = myDescr1.CompareDescriptor(inputMatrix2,descriptors2);//ч/б итоговое изображение
-    QImage result = myDescr1.CompareDescriptor(image1,image2,descriptors2);//цветное итоговое изображение
+    QImage result = myDescr1.CompareDescriptor(inputMatrix2,descriptors2);//ч/б итоговое изображение
+   // QImage result = myDescr1.CompareDescriptor(image1,image2,descriptors2);//цветное итоговое изображение
     result.save(labPath+"общие дескрипторы чб.jpg");
     Print("\n************************************************************************************************\n");
 }
@@ -39,17 +39,21 @@ void lab67(QString path,QString fileName1, QString ext1,QString fileName2, QStri
 
 int main()
 {
-//        lab67("D:\\ComputerVision\\Lab6-7\\","elf",".jpg","elf2",".jpg",3,2000,8,8,4);
-//        lab67("D:\\ComputerVision\\Lab6-7\\","elf",".jpg","elf3",".jpg",3,2000,8,8,4);
-//        lab67("D:\\ComputerVision\\Lab6-7\\","elf",".jpg","elf4",".jpg",3,2000,8,8,4);
+        lab67("D:\\ComputerVision\\Lab6-7\\","elf",".jpg","elf2",".jpg",3,2000,8,8,4);
+        lab67("D:\\ComputerVision\\Lab6-7\\","elf",".jpg","elf3",".jpg",3,2000,8,8,4);
+        lab67("D:\\ComputerVision\\Lab6-7\\","elf",".jpg","elf4",".jpg",3,2000,8,8,4);
 
-//        lab67("D:\\ComputerVision\\Lab6-7\\","frog",".jpg","frog2",".jpg",3,2000,8,8,4);
-//        lab67("D:\\ComputerVision\\Lab6-7\\","frog",".jpg","frog3",".jpg",3,2000,8,8,4);
-//        lab67("D:\\ComputerVision\\Lab6-7\\","frog",".jpg","frog4",".jpg",3,2000,8,8,4);
+        lab67("D:\\ComputerVision\\Lab6-7\\","frog",".jpg","frog2",".jpg",3,2000,8,8,4);
+        lab67("D:\\ComputerVision\\Lab6-7\\","frog",".jpg","frog3",".jpg",3,2000,8,8,4);
+        lab67("D:\\ComputerVision\\Lab6-7\\","frog",".jpg","frog4",".jpg",3,2000,8,8,4);
 
-//        lab67("D:\\ComputerVision\\Lab6-7\\","girl",".jpg","girl2",".jpg",3,2000,8,8,4);
-//        lab67("D:\\ComputerVision\\Lab6-7\\","girl",".jpg","girl3",".jpg",3,2000,8,8,4);
-//        lab67("D:\\ComputerVision\\Lab6-7\\","girl",".jpg","girl4",".jpg",3,2000,8,8,4);
+        lab67("D:\\ComputerVision\\Lab6-7\\","girl",".jpg","girl2",".jpg",3,2000,8,8,4);
+        lab67("D:\\ComputerVision\\Lab6-7\\","girl",".jpg","girl3",".jpg",3,2000,8,8,4);
+        lab67("D:\\ComputerVision\\Lab6-7\\","girl",".jpg","girl4",".jpg",3,2000,8,8,4);
+
+        lab67("D:\\ComputerVision\\Lab6-7\\","bridge",".jpg","bridge2",".jpg",3,2000,8,8,4);
+
+        lab67("D:\\ComputerVision\\Lab6-7\\","library",".jpg","library2",".jpg",3,2000,8,8,4);
 
         lab67("D:\\ComputerVision\\Lab6-7\\","Queen",".jpg","Queen2",".jpg",3,2000,8,8,4);
 
@@ -57,7 +61,7 @@ int main()
 
         lab67("D:\\ComputerVision\\Lab6-7\\","Moscow",".jpg","Moscow2",".jpg",3,2000,8,8,4);
 
-        lab67("D:\\ComputerVision\\Lab6-7\\","Tuz",".jpg","Tuz2",".jpg",3,2000,8,8,4);
+
 
      Print("\nВыполнение успешно завершено.\n");
     return 0;
